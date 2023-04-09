cbdc7c942b0cd68a53acc757f3cd3eb0dd0698e0
Hanauta <85512947+Hanautas@users.noreply.github.com>
2023-04-09 21:58:53 +0200

    Looking Added

---
+++ VR Project/Assets/_Scripts/Looker.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRM;
+
+public class Looker : MonoBehaviour
+{
+	public GameObject head;
+
+    public VRMLookAtHead lookAtHead;
+
+    public List<GameObject> nearbyObjects;
+
+    void Update()
+    {
+        FindNearestObject();
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Object")
+        {
+            if (GetTarget() == null)
+            {
+                SetTarget(other.gameObject);
+            }
+
+            if (!nearbyObjects.Contains(other.gameObject))
+            {
+                nearbyObjects.Add(other.gameObject);
+            }
+        }
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        if (other.gameObject.tag == "Object")
+        {
+            if (nearbyObjects.Contains(other.gameObject))
+            {
+                nearbyObjects.Remove(other.gameObject);
+            }
+
+            if (other.gameObject == GetTarget())
+            {
+                FindNearestObject();
+            }
+        }
+	}
+
+	private void FindNearestObject()
+	{
+		if (nearbyObjects.Count == 0)
+		{
+			SetTarget(null);
+		}
+		else if (nearbyObjects.Count == 1)
+		{
+			SetTarget(nearbyObjects[0]);
+		}
+		else
+		{
+			SetTarget(nearbyObjects[0]);
+
+			var nearestObjectDistance = GetDistance(head.transform.position, GetTarget().transform.position);
+
+			for (int i = 1; i < nearbyObjects.Count; i++)
+			{
+				if (GetDistance(head.transform.position, nearbyObjects[i].transform.position) < nearestObjectDistance)
+				{
+					SetTarget(nearbyObjects[i]);
+
+					nearestObjectDistance = GetDistance(head.transform.position, GetTarget().transform.position);
+				}
+			}
+		}	
+	}
+
+	private float GetDistance(Vector3 a, Vector3 b)
+	{
+		return (a - b).sqrMagnitude;
+	}
+
+    private Transform GetTarget()
+    {
+        return lookAtHead.Target;
+    }
+
+    private void SetTarget(GameObject target)
+    {
+        lookAtHead.Target = target.transform;
+    }
+}
