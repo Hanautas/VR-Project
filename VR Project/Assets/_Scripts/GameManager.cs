8ad40abcefffee620e1259db86d0a596dcb842ca
Hanauta <85512947+Hanautas@users.noreply.github.com>
2023-06-01 00:15:27 +0200

    Update

--- VR Project/Assets/_Scripts/GameManager.cs
+++ VR Project/Assets/_Scripts/GameManager.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.XR.CoreUtils;
 using UnityEngine.SceneManagement;
+using EVMC4U;
 
 public class GameManager : MonoBehaviour
 {
+    public ExternalReceiver externalReceiver;
+
     [Header("UI")]
     public GameObject playerMenu;
 
     [Header("Avatar")]
     public Transform playerAvatar;
+
+    public List<GameObject> avatars;
 
     [Header("Position")]
     public Transform origin;
@@ -46,6 +51,18 @@ public class GameManager : MonoBehaviour
         playerAvatar.localScale = new Vector3(playerSize, playerSize, playerSize);
 
         playerData.playerSize = playerSize;
+    }
+
+    public void SetPlayerAvatar(int index)
+    {
+        foreach (GameObject obj in avatars)
+        {
+            obj.SetActive(false);
+        }
+
+        avatars[index].SetActive(true);
+
+        externalReceiver.Model = avatars[index];
     }
 
     public void LoadScene(string sceneName)
